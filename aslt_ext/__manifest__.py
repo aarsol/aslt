b50c09aaed1d7062a31496f4510b59729bebdee5
farooq <farooq@aarsol.com>
2020-03-29 18:50:14 +0500

    Commit

--- aslt_ext/__manifest__.py
+++ aslt_ext/__manifest__.py
@@ -1,27 +1,19 @@
 # -*- coding: utf-8 -*-
 {
-    'name': "aslt_ext",
-
+    'name': "ASLT",
     'summary': """
-        Short (1 phrase/line) summary of the module's purpose, used as
-        subtitle on modules listing or apps.openerp.com""",
+        Extension for ASLT Project""",
 
     'description': """
-        Long description of module's purpose
+        Extension for ASLT Project
     """,
 
-    'author': "My Company",
-    'website': "http://www.yourcompany.com",
-
-    # Categories can be used to filter modules in modules listing
-    # Check https://github.com/odoo/odoo/blob/13.0/odoo/addons/base/data/ir_module_category_data.xml
-    # for the full list
-    'category': 'Uncategorized',
+    'author': "Numan Ali",
+    'website': "http://www.aarsol.com",
+    'category': 'Accounts',
     'version': '0.1',
     'sequence': 2,
-
-    # any module necessary for this one to work correctly
-    'depends': ['base','account','sale'],
+    'depends': ['base', 'account', 'sale'],
 
     # always loaded
     'data': [
@@ -30,12 +22,10 @@
         'views/weekly_payment_view.xml',
         'views/account_journal_ext_view.xml',
         'views/customer_ext_view.xml',
+        'views/templates.xml',
+        'data/invoice_mail_template.xml',
 
-    ],
-    # only loaded in demonstration mode
-    'demo': [
-        'demo/demo.xml',
-    ],
+    ],    
     "installable": True,
     "auto_install": False,
     'application': True,
--- aslt_ext/controllers/controllers.py
+++ aslt_ext/controllers/controllers.py
@@ -1,21 +1,58 @@
 # -*- coding: utf-8 -*-
-# from odoo import http
+from odoo import http, _
+from odoo.addons.portal.controllers.portal import CustomerPortal, pager as portal_pager
+from odoo.exceptions import AccessError, MissingError
+from odoo.http import request
 
 
-# class AsltExt(http.Controller):
-#     @http.route('/aslt_ext/aslt_ext/', auth='public')
-#     def index(self, **kw):
-#         return "Hello, world"
+class AsltExt(http.Controller):
+    @http.route('/aslt_ext/invoice_move/<int:id>', type='http', auth="public", website=True)
+    def accept(self,id=0, **kw):
 
-#     @http.route('/aslt_ext/aslt_ext/objects/', auth='public')
-#     def list(self, **kw):
-#         return http.request.render('aslt_ext.listing', {
-#             'root': '/aslt_ext/aslt_ext',
-#             'objects': http.request.env['aslt_ext.aslt_ext'].search([]),
-#         })
+        invoice = http.request.env['account.move'].sudo().search([('id','=',id)])
+        if invoice.marked_user_id and invoice.marked_state == 'draft':
+            invoice.update({'marked_state':'accept'})
+            values = {
+                'header': 'Action Done Successfully  !',
+                'message': 'Your Action is Accept Successfully.\n Thank you.'
+            }
+            return request.render("aslt_ext.portal_submission_message", values)
+        else:
+            values = {
+                'header': 'Duplicate Error!',
+                'message': 'Your Action is already been entertained! You can check on view .\n Thank you.'
+            }
+            return request.render("aslt_ext.portal_submission_message", values)
 
-#     @http.route('/aslt_ext/aslt_ext/objects/<model("aslt_ext.aslt_ext"):obj>/', auth='public')
-#     def object(self, obj, **kw):
-#         return http.request.render('aslt_ext.object', {
-#             'object': obj
-#         })
+
+    @http.route('/aslt_ext/invoice_move_reject/<int:id>', auth='public')
+    def reject(self, id=0, **kw):
+
+        invoice = http.request.env['account.move'].sudo().search([('id', '=', id)])
+        if invoice.marked_user_id and invoice.marked_state == 'draft':
+            invoice.update({'marked_state': 'cancel'})
+            values = {
+                'header': 'Action Done Successfully  !',
+                'message': 'Your Action is cancel Successfully.\n Thank you.'
+            }
+            return request.render("aslt_ext.portal_submission_message", values)
+        else:
+            values = {
+                'header': 'Duplicate Error!',
+                'message': 'Your Action is already been entertained! You can check on view .\n Thank you.'
+            }
+            return request.render("aslt_ext.portal_submission_message", values)
+
+
+    # @http.route('/aslt_ext/aslt_ext/objects/', auth='public')
+    # def list(self, **kw):
+    #     return http.request.render('aslt_ext.listing', {
+    #         'root': '/aslt_ext/aslt_ext',
+    #         'objects': http.request.env['aslt_ext.aslt_ext'].search([]),
+    #     })
+    #
+    # @http.route('/aslt_ext/aslt_ext/objects/<model("aslt_ext.aslt_ext"):obj>/', auth='public')
+    # def object(self, obj, **kw):
+    #     return http.request.render('aslt_ext.object', {
+    #         'object': obj
+    #     })
