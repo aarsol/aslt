3151b5755f917e46e8c205daf253171515d97a59
farooq <farooq@aarsol.com>
2020-04-12 01:40:25 +0500

    Commit

---
+++ aslt_ext/controllers/portal.py
@@ -0,0 +1,59 @@
+# -*- coding: utf-8 -*-
+
+import binascii
+
+from odoo import fields, http, _
+from odoo.addons.portal.controllers.portal import CustomerPortal, pager as portal_pager
+from odoo.exceptions import AccessError, MissingError
+from odoo.http import request
+from odoo.addons.portal.controllers.portal import CustomerPortal, pager as portal_pager, get_records_pager
+from odoo.osv import expression
+from odoo.addons.portal.controllers.mail import _message_post_helper
+import pdb
+
+class CustomerPortal(CustomerPortal):
+    @http.route(['/my/orders/<int:order_id>/accept'], type='json', auth="public", website=True)
+    def portal_quote_accept(self, order_id, access_token=None, name=None, signature=None, payment_methods=None):
+        # get from query string if not on json param
+
+        access_token = access_token or request.httprequest.args.get('access_token')
+        try:
+            order_sudo = self._document_check_access('sale.order', order_id, access_token=access_token)
+        except (AccessError, MissingError):
+            return {'error': _('Invalid order.')}
+
+        if not order_sudo.has_to_be_signed():
+            return {'error': _('The order is not in a state requiring customer signature.')}
+        if not signature:
+            return {'error': _('Signature is missing.')}
+
+        try:
+            data_sale_order = {
+                'signed_by': name,
+                'signed_on': fields.Datetime.now(),
+                'signature': signature,
+            }
+            if payment_methods and payment_methods !="":
+                data_sale_order['payment_methods'] = payment_methods
+            order_sudo.write(data_sale_order)
+        except (TypeError, binascii.Error) as e:
+            return {'error': _('Invalid signature data.')}
+
+        if not order_sudo.has_to_be_paid():
+            order_sudo.action_confirm()
+            order_sudo._send_order_confirmation_mail()
+
+        pdf = request.env.ref('sale.action_report_saleorder').sudo().render_qweb_pdf([order_sudo.id])[0]
+
+        _message_post_helper(
+            'sale.order', order_sudo.id, _('Order signed by %s') % (name,),
+            attachments=[('%s.pdf' % order_sudo.name, pdf)],
+            **({'token': access_token} if access_token else {}))
+
+        query_string = '&message=sign_ok'
+        if order_sudo.has_to_be_paid(True):
+            query_string += '#allow_payment=yes'
+        return {
+            'force_refresh': True,
+            'redirect_url': order_sudo.get_portal_url(query_string=query_string),
+        }
