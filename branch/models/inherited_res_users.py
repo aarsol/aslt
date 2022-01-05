e508eaa30da2f0b3046b5b4ec33bf4cdbae17962
sarfraz <sarfraz_g2009@yahoo.com>
2022-01-05 15:35:41 +0500

    Commit Done

--- branch/models/inherited_res_users.py
+++ branch/models/inherited_res_users.py
@@ -6,10 +6,8 @@ from odoo import api, fields, models, _
 class ResUsers(models.Model):
     _inherit = 'res.users'
 
-    branch_ids = fields.Many2many('res.branch',string="Allowed Branch")
-    branch_id = fields.Many2one('res.branch', string= 'Branch')
-
-
+    branch_ids = fields.Many2many('res.branch', string="Allowed Branch")
+    branch_id = fields.Many2one('res.branch', string='Branch')
 
     def write(self, values):
         if 'branch_id' in values or 'branch_ids' in values:
